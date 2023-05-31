3ca226aa44379c2711f6c96642637ddedfc4eff3
Haren Pylla <haren.pylla@gmail.com>
2023-05-30 20:55:24 -0400

    All pages main setup is done

--- src/App.js
+++ src/App.js
@@ -1,6 +1,21 @@
 import "./App.css";
+import AboutUs from "./Sections/AboutUs";
+import Contact from "./Sections/Contact";
+import Description from "./Sections/Description";
+import Example from "./Sections/Example";
+import Navbar from "./Sections/Navbar";
+import Title from "./Sections/Title";
 function App() {
-  return <h1 className="text-3xl font-bold underline">Hello world!</h1>;
+  return (
+    <div>
+      <Navbar />
+      <Title />
+      <Description />
+      <Example />
+      <AboutUs />
+      <Contact />
+    </div>
+  );
 }
 
 export default App;
