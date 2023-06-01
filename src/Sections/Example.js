ca451baf7d7c7819ef1254ac2afb2cb2944a7618
Sahil K <skhunt125@gmail.com>
2023-05-31 21:53:40 -0500

    Found Template
    Got a template from tailwind for how this section should look. I'll get the formatting first so we can add the content last.

--- src/Sections/Example.js
+++ src/Sections/Example.js
@@ -1,7 +1,76 @@
+//Above this section say something like "We aere the right team for you"
+//Then this section will start off by saying "Don't believe us, take a look at our past works"
+
 import React from "react";
+import "./Example.css";
+
+const callouts = [
+  {
+    name: "Scientific Laboratory",
+    description: "A lab based in Mumbai, India",
+    imageSrc:
+      "https://tailwindui.com/img/ecommerce-images/home-page-02-edition-01.jpg",
+    imageAlt: "Write a decription here if the image doesn't load.",
+    href: "#",
+  },
+  {
+    name: "Financing Company",
+    description: "An up-and-coming accounting firm",
+    imageSrc:
+      "https://tailwindui.com/img/ecommerce-images/home-page-02-edition-02.jpg",
+    imageAlt: "Write a decription here if the image doesn't load.",
+    href: "#",
+  },
+  {
+    name: "Local Chicago Business",
+    description: "One of the local businesses we're currently working with",
+    imageSrc:
+      "https://tailwindui.com/img/ecommerce-images/home-page-02-edition-03.jpg",
+    imageAlt: "Write a decription here if the image doesn't load.",
+    href: "#",
+  },
+];
 
 function Example() {
-  return <div>Example</div>;
+  return (
+    <div className="wholeSection">
+      <p className="text-center text-3xl font-bold tracking-tight text-black sm:text-5xl">
+        Don't Believe Us Yet?
+      </p>
+      <p className="">
+        Check out some of our past works from our satisfied customers
+      </p>
+
+      <div className="mx-auto max-w-7xl px-4 sm:px-6 lg:px-8">
+        <div className="mx-auto max-w-2xl py-16 sm:py-24 lg:max-w-none lg:py-32">
+          {/* <h2 className="text-2xl font-bold text-gray-900">Collections</h2> */}
+
+          <div className="mt-6 space-y-12 lg:grid lg:grid-cols-3 lg:gap-x-6 lg:space-y-0">
+            {callouts.map((callout) => (
+              <div key={callout.name} className="group relative">
+                <div className="relative h-80 w-full overflow-hidden rounded-lg bg-white sm:aspect-h-1 sm:aspect-w-2 lg:aspect-h-1 lg:aspect-w-1 group-hover:opacity-75 sm:h-64">
+                  <img
+                    src={callout.imageSrc}
+                    alt={callout.imageAlt}
+                    className="h-full w-full object-cover object-center p-2"
+                  />
+                </div>
+                <h3 className="mt-6 text-sm text-gray-500">
+                  <a href={callout.href}>
+                    <span className="absolute inset-0" />
+                    {callout.name}
+                  </a>
+                </h3>
+                <p className="text-base font-semibold text-gray-900">
+                  {callout.description}
+                </p>
+              </div>
+            ))}
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default Example;
