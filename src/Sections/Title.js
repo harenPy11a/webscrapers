18d4ba132b0a3576f404f80d5ab58ed5d277b05d
Haren Pylla <haren.pylla@gmail.com>
2023-06-04 16:23:02 -0500

    Section is full screen

--- src/Sections/Title.js
+++ src/Sections/Title.js
@@ -2,7 +2,7 @@ import React from "react";
 
 function Title() {
   return (
-    <section>
+    <section className="h-screen">
       <div class="grid max-w-screen-xl px-4 py-8 mx-auto lg:gap-8 xl:gap-0 lg:py-20 lg:grid-cols-12">
         <div class="mr-auto place-self-center lg:col-span-7">
           <h1 class="max-w-2xl mb-4 text-4xl font-extrabold tracking-tight leading-none md:text-5xl xl:text-6xl dark:text-white">
