cafacd934f654e6b9a7d09b90215d700f6f4e3a9
Vedant P <vpat1621@gmail.com>
2023-06-08 20:51:14 -0500

    ExampleScreen fonts are done

--- src/Sections/Example.js
+++ src/Sections/Example.js
@@ -2,6 +2,9 @@
 //Then this section will start off by saying "Don't believe us, take a look at our past works"
 
 import React from "react";
+import labImg from './Assets/pexels-rodolfo-clix-1366942.jpg'
+import financeImg from './Assets/pexels-pixabay-265087.jpg'
+import chicagoImg from './Assets/pexels-bhargava-marripati-1569012.jpg'
 // import "./Example.css";
 
 const callouts = [
@@ -9,7 +12,7 @@ const callouts = [
     name: "Scientific Laboratory",
     description: "A lab based in Mumbai, India",
     imageSrc:
-      "https://tailwindui.com/img/ecommerce-images/home-page-02-edition-01.jpg",
+      labImg,
     imageAlt: "Write a decription here if the image doesn't load.",
     href: "#",
   },
@@ -17,7 +20,7 @@ const callouts = [
     name: "Financing Company",
     description: "An up-and-coming accounting firm",
     imageSrc:
-      "https://tailwindui.com/img/ecommerce-images/home-page-02-edition-02.jpg",
+      financeImg,
     imageAlt: "Write a decription here if the image doesn't load.",
     href: "#",
   },
@@ -25,7 +28,7 @@ const callouts = [
     name: "Local Chicago Business",
     description: "One of the local businesses we're currently working with",
     imageSrc:
-      "https://tailwindui.com/img/ecommerce-images/home-page-02-edition-03.jpg",
+      chicagoImg,
     imageAlt: "Write a decription here if the image doesn't load.",
     href: "#",
   },
@@ -34,15 +37,14 @@ const callouts = [
 function Example() {
   return (
     <div className="h-500 bg-[#695B55]">
-      <p className="text-center font-semibold tracking-tight sm:text-6xl text-white pt-14">
+      <p className="text-center font-semibold tracking-tight text-4xl md:text-5xl xl:text-6xl text-white pt-14">
         Don't Believe Us Yet?
       </p>
-      <p className="text-center sm:text-2xl tracking-tight font-semibold text-white pt-5">
+      <p className="text-center text-lg md:text-xl xl:text-2xl tracking-tight text-white pt-5">
         Check out some of our past works from our satisfied customers
       </p>
       <div className="mx-auto max-w-7xl px-4 sm:px-6 lg:px-5 pb-20">
         <div className="mx-auto max-w-2xl pt-16 lg:max-w-none ">
-          {/* <h2 className="text-2xl font-bold text-gray-900">Collections</h2> */}
 
           <div className="mt-6 space-y-12 lg:grid lg:grid-cols-3 lg:gap-x-6 lg:space-y-0">
             {callouts.map((callout) => (
@@ -54,13 +56,13 @@ function Example() {
                     className="h-full w-full object-cover object-center p-2"
                   />
                 </div>
-                <h3 className="sm:text-4xl text-white pl-1 pt-8 text-base font-semibold text-gray-900">
+                <h3 className="text-3xl md:text-4xl text-white pl-1 pt-8 font-bold text-gray-900">
                   <a href={callout.href}>
                     <span className="absolute inset-0" />
                     {callout.name}
                   </a>
                 </h3>
-                <p className="sm:text-lg font-semibold text-white pl-1 mt-6 text-sm text-gray-500">
+                <p className="text-xl md:text-xl lg:text-xl xl:text-2xl text-white pl-1 mt-6 text-gray-500">
                   {callout.description}
                 </p>
               </div>
