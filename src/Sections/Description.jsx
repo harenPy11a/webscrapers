1bb5dacaed82485034a68398e27db693ed589e6c
DumplingDefender <82426255+DumplingDefender@users.noreply.github.com>
2023-11-23 14:40:59 -0600

    change description block color

--- src/Sections/Description.jsx
+++ src/Sections/Description.jsx
@@ -5,6 +5,8 @@ import desc2 from "./Assets/desc2.jpg";
 import desc5 from "./Assets/desc5.jpg";
 import desc4 from "./Assets/desc4.jpg";
 import desc6 from "./Assets/desc6.jpg";
+import NavBlock from "./Navbar";
+
 
 
 const features = [
@@ -42,7 +44,9 @@ const features = [
 
 const DescriptionBlock = block(function Description() {
   return (
-    <div id="description" className="bg-[#fffdf5] relative">
+    <div id="description" className="bg-[#9e9791] relative">
+      {/* <NavBlock /> */}
+
       <div className="mx-auto grid max-w-screen grid-cols-1 lg:items-center gap-x-8 gap-y-4 lg:gap-y-16 px-4 sm:px-6 pt-8 lg:py-32 lg:max-w-7xl lg:grid-cols-2 lg:px-8">
         <div>
           <h2 className="text-3xl lg:text-5xl font-bold tracking-tight text-grey-900 text-black sm:text-center mt-4 lg:mt-0 ">
@@ -50,19 +54,19 @@ const DescriptionBlock = block(function Description() {
           </h2>
           <p className="mt-4 lg:text-2xl text-xl text-black">
             At Webscrapers, our focus remains on{" "}
-            <span class="font-bold text-[#695B55]">your</span> needs. We strive
+            <span class="font-bold text-black">your</span> needs. We strive
             to fulfill the goals of{" "}
-            <span class="font-bold text-[#695B55]">your</span> website in the
-            way that <span class="font-bold text-[#695B55]">you</span> want it.
+            <span class="font-bold text-black">your</span> website in the
+            way that <span class="font-bold text-black">you</span> want it.
           </p>
 
           <dl className="mt-10 grid grid-cols-1 gap-x-6 gap-y-10 sm:grid-cols-2 sm:gap-y-16 lg:gap-x-8">
             {features.map((feature) => (
               <div
                 key={feature.name}
-                className="border-b-2 border-gray-600 pb-4 text-[#695B55]"
+                className="border-b-2 border-gray-600 pb-4 text-black"
               >
-                <dt className="font-extrabold text-lg text-[#695B55]}">
+                <dt className="font-extrabold text-lg text-black}">
                   {feature.name}
                 </dt>
                 <dd className="mt-2 text-md text-black">
@@ -73,7 +77,7 @@ const DescriptionBlock = block(function Description() {
           </dl>
         </div>
         <div className="grid grid-cols-2 grid-rows-2 gap-4 sm:gap-6 lg:gap-8">
-        
+
           <div className="invisible lg:visible h-0 lg:h-full">
             <img
               src={desc4}
